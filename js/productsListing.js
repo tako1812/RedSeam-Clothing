8d63efbb7854d2a77fc20581c99c073cba83ac50
Tamar Robakidze <troba12@freeuni.edu.ge>
2025-09-21 04:58:37 +0400

    feat: add pagination functionality

--- js/productsListing.js
+++ js/productsListing.js
@@ -119,16 +119,17 @@ const switchActiveContainer = function(e) {
 headingControlsContainer.addEventListener("click", switchActiveContainer);
 
 
-
-
-
+/*
+const paginationContainer =document.querySelector(".pagination-container__pages");
+let datas;
 const clothingListingContainer = document.querySelector(".clothing-listing");
-const renderListing = async function () {
+
+const renderListingWithPagination = async function () {
     clothingListingContainer.innerHTML = "";
   const res = await fetch(
     "https://api.redseam.redberryinternship.ge/api/products"
   );
-  const datas = await res.json();
+  datas = await res.json();
   console.log(datas);
 
   datas.data.map(data => {
@@ -139,8 +140,119 @@ const renderListing = async function () {
             <p>$ ${data.price}</p>
         </div>
         `;
-
         clothingListingContainer.insertAdjacentHTML("afterbegin", html);
     });
+
+        const html = `
+            <div class="page" data-page="1">1</div>
+            <div class="page" data-page="2">2</div>
+            <div class="dots">...</div>
+            <div class="page" data-page="${datas.meta.last_page - 1}">${datas.meta.last_page - 1}</div>
+            <div class="page" data-page="${datas.meta.last_page}">${datas.meta.last_page}</div>
+        `;
+        paginationContainer .innerHTML = html;
 };
-renderListing();
+renderListingWithPagination();
+
+
+
+const switchPages = function() {
+
+    let currentPage = 1; 
+    //const lastPage = datas.meta.last_page; 
+
+
+    const pageNum = e.target.closest(".page");
+    if (!pageNum) return;
+    const page = Number(pageBtn.dataset.page);
+    currentPage = page;
+    fetchAndRenderPage(currentPage); // call your function to fetch data for that page
+}
+paginationContainer.addEventListener("click", renderPaginationPages);
+*/
+
+
+
+
+
+
+const paginationContainer =document.querySelector(".pagination-container__pages");
+let datas;
+const clothingListingContainer = document.querySelector(".clothing-listing");
+let currentPage = 1;
+//let currentPage = 1; // keep it outside so it persists
+
+
+const renderListingWithPagination = async function (page = 1) {
+    clothingListingContainer.innerHTML = "";
+  const res = await fetch(
+    (`https://api.redseam.redberryinternship.ge/api/products?page=${page}`)
+    );
+  datas = await res.json();
+  console.log(datas);
+
+  datas.data.map(data => {
+        const html = `
+        <div class="clothing-listing__card">
+            <img src="${data.cover_image}">
+            <p>${data.name}</p>
+            <p>$ ${data.price}</p>
+        </div>
+        `;
+        clothingListingContainer.insertAdjacentHTML("afterbegin", html);
+    });
+
+      /* const html = `
+            <div class="page" data-page="1">1</div>
+            <div class="page" data-page="2">2</div>
+            <div class="dots">...</div>
+            <div class="page" data-page="${datas.meta.last_page - 1}">${datas.meta.last_page - 1}</div>
+            <div class="page" data-page="${datas.meta.last_page}">${datas.meta.last_page}</div>
+        `;
+        paginationContainer .innerHTML = html;*/
+
+
+
+
+    const lastPage = datas.meta.last_page;
+
+    let html = '';
+    for(let i = 1; i <= lastPage; i++) {
+        html += `<div  class="page ${currentPage === i ? "active" : ""}" data-page="${i}">${i}</div>`;
+
+        if(i > 1 && i < lastPage - 2) {
+            html += `<div class="dots">...</div>`;
+            i = lastPage - 2; 
+        }
+    };
+    
+    paginationContainer.innerHTML = html;
+
+};
+renderListingWithPagination();
+    
+    
+
+const switchPages = function(e) {
+
+    let cuurrentPage = 1;
+    console.log(e.target);
+    
+    if(e.target.classList.contains("page")){
+        const pageNum = e.target;
+        if (!pageNum) return;
+        const page = Number(pageNum.dataset.page);
+        if (page === currentPage) return;
+    
+        currentPage = page;
+        renderListingWithPagination(currentPage);
+
+
+
+    }
+ 
+    
+}
+paginationContainer.addEventListener("click", switchPages);
+
+
